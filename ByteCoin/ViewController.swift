ff2bd27228a64e5ae2631bc21774098becb4e76c
Igor Guryan <sacujo93@yandex.ru>
2024-08-11 01:12:35 +0300

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Igor Guryan on 11.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
